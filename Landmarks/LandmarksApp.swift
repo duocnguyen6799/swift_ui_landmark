304d028735aa48375c93e5f7b57ba7b9a01e5556
duocnp <duocnguyen6799@gmail.com>
2022-10-12 14:54:10 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Nguyen Phuc Duoc on 12/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
